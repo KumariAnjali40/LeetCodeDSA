95348b1aba0e9765abd1e48ee1300c94cc46abc7
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-21 21:17:07 +0530

    Time: 7 ms (38.88%), Space: 53.7 MB (8.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,27 +1,48 @@
 class Solution {
     public int majorityElement(int[] nums) {
-         HashMap<Integer, Integer> map = new HashMap<>();
+        //  HashMap<Integer, Integer> map = new HashMap<>();
 
-         for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i])+1);
-            }else {
-                map.put(nums[i], 1);
-            }
-         }
+        //  for(int i = 0; i < nums.length; i++){
+        //     if(map.containsKey(nums[i])){
+        //         map.put(nums[i], map.get(nums[i])+1);
+        //     }else {
+        //         map.put(nums[i], 1);
+        //     }
+        //  }
 
-         int max = Integer.MIN_VALUE;
-         int ans = nums[0];
+        //  int max = Integer.MIN_VALUE;
+        //  int ans = nums[0];
 
-         for(Integer key : map.keySet()){
+        //  for(Integer key : map.keySet()){
             
-                if(map.get(key) > max){
-                    ans = key;
-                    max = map.get(key);
-                } 
-             
-         }
+        //         if(map.get(key) > max){
+        //             ans = key;
+        //             max = map.get(key);
+        //         } 
+        //  }
 
-         return ans;
+        //  return ans;
+
+        // 2 3 3 
+
+        Arrays.sort(nums);
+
+        int count = 1; 
+        int ans = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+                if(count > nums.length/2){
+                    ans = nums[i];
+                }
+            }else {
+               count = 1;
+            }
+        }
+
+
+        return ans;
+
     }
 }
