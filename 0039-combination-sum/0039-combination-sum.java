8b1701f034626857af2bbbad93fc767372eeb1f4
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-25 17:14:18 +0530

    Time: 2 ms (83.41%), Space: 44.8 MB (30.73%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,27 +2,44 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
 
-        findCombination(candidates,target,0, new ArrayList<>(), ans);
+        // findCombination(candidates,target,0, new ArrayList<>(), ans);
+
+         findCombination(candidates,target,0,0, new ArrayList<>(), ans);
 
         return ans;
     }
 
-    public static void findCombination(int [] arr, int target, int index,List<Integer>curr, List<List<Integer>>ans ){
+    public static void findCombination(int [] arr, int target,int index,int sum, List<Integer>curr, List<List<Integer>>ans ){
 
-        if(index == arr.length){
-            if(target == 0){
-                ans.add(new ArrayList<>(curr));
-            }
+        // if(index == arr.length){
+        //     if(target == 0){
+        //         ans.add(new ArrayList<>(curr));
+        //     }
+        //     return;
+        // }
+
+        // if(arr[index] <= target){
+        //     curr.add(arr[index]); 
+        //     findCombination(arr, target-arr[index], index, curr, ans);
+
+        //     curr.remove(curr.size()-1);
+        // }
+
+        // findCombination(arr, target, index+1, curr, ans);
+
+        if(sum == target){
+            ans.add(new ArrayList<>(curr));
             return;
         }
 
-        if(arr[index] <= target){
-            curr.add(arr[index]); 
-            findCombination(arr, target-arr[index], index, curr, ans);
-
-            curr.remove(curr.size()-1);
+        if(sum > target || index == arr.length){
+            return;
         }
 
-        findCombination(arr, target, index+1, curr, ans);
+
+        curr.add(arr[index]);
+        findCombination(arr, target, index, sum+arr[index], curr, ans);
+        curr.remove(curr.size()-1);
+        findCombination(arr, target, index+1, sum, curr, ans);
     }
 }
