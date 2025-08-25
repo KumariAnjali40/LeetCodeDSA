441744b0e0ce46e7cbd7ba2d5c7bb965c11796e3
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-26 01:21:44 +0530

    Time: 3 ms (20.77%), Space: 43.6 MB (44.76%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -2,31 +2,52 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
 
-        List<List<Integer>> result = new ArrayList<>();
+        // List<List<Integer>> result = new ArrayList<>();
 
-        subset(nums,0,new ArrayList<>(),result);
+        // subset(nums,0,new ArrayList<>(),result);
 
-        return result;
+        // return result;
+
+        Set<ArrayList<Integer>> set = new HashSet<>();
+
+        subset(nums, 0, set, new ArrayList<>());
+
+        return new ArrayList<>(set);
     }
 
-    public static void subset(int [] arr, int currIndex, ArrayList<Integer> current, List<List<Integer>> result ){
-           if(currIndex >= arr.length){
-            result.add(new ArrayList<>(current));
+    public static void subset(int [] arr,int index, Set<ArrayList<Integer>>ans, ArrayList<Integer>curr){
+        if(index >= arr.length){
+            ans.add(new ArrayList<>(curr));
+
             return;
-           }
+        }
 
-           current.add(arr[currIndex]);
-           subset(arr,currIndex+1,current,result);
+        curr.add(arr[index]);
+        subset(arr, index+1,ans, curr);
 
-           current.remove(current.size()-1);
+        curr.remove(curr.size()-1);
 
-           int nextIndex = currIndex+1;
-
-           while(nextIndex < arr.length && arr[currIndex] == arr[nextIndex]){
-            nextIndex++;
-           }
-
-           subset(arr,nextIndex,current,result);
-
+        subset(arr, index+1, ans, curr);
     }
+
+    // public static void subset(int [] arr, int currIndex, ArrayList<Integer> current, List<List<Integer>> result ){
+    //        if(currIndex >= arr.length){
+    //         result.add(new ArrayList<>(current));
+    //         return;
+    //        }
+
+    //        current.add(arr[currIndex]);
+    //        subset(arr,currIndex+1,current,result);
+
+    //        current.remove(current.size()-1);
+
+    //        int nextIndex = currIndex+1;
+
+    //        while(nextIndex < arr.length && arr[currIndex] == arr[nextIndex]){
+    //         nextIndex++;
+    //        }
+
+    //        subset(arr,nextIndex,current,result);
+
+    // }
 }
