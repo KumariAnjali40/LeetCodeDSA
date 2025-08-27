590ae76da874a2b13ae35a90755f21894f9b7bc2
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-27 16:23:05 +0530

    Time: 5 ms (56.15%), Space: 42.8 MB (97.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -17,23 +17,43 @@ class Solution {
 
         // return m1.equals(m2);
 
-        if(s.length() != t.length()) return false;
+        //second solution
 
-        int [] sarr = new int [26];
-        int [] tarr = new int [26];
+        // if(s.length() != t.length()) return false;
+
+        // int [] sarr = new int [26];
+        // int [] tarr = new int [26];
+
+        // for(int i = 0; i < s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     sarr[ch -  'a']++;
+        // }
+
+        // for(int i = 0; i < t.length(); i++){
+        //     char ch = t.charAt(i);
+        //     tarr[ch -  'a']++;
+        // }
+
+        // for(int i = 0; i < 26; i++){
+        //     if(tarr[i] != sarr[i]) return false;
+        // }
+
+        // return true;
+
+        //more optimse 
+
+        if(s.length() != t.length()) return false;
+        int [] arr = new int [26];
 
         for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            sarr[ch -  'a']++;
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
 
-        for(int i = 0; i < t.length(); i++){
-            char ch = t.charAt(i);
-            tarr[ch -  'a']++;
-        }
-
-        for(int i = 0; i < 26; i++){
-            if(tarr[i] != sarr[i]) return false;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0){
+                return false;
+            }
         }
 
         return true;
