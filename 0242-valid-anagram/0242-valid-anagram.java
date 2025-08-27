be5902ebb2bf22fd518a01394c04c3c4601c87a5
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-27 16:19:46 +0530

    Time: 4 ms (77.21%), Space: 43.6 MB (59.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,41 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> m1 = new HashMap<>();
-        HashMap<Character, Integer> m2 = new HashMap<>();
+        // HashMap<Character, Integer> m1 = new HashMap<>();
+        // HashMap<Character, Integer> m2 = new HashMap<>();
+
+        // if(s.length() != t.length()) return false;
+
+        // for(int i = 0; i < s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     m1.put(ch, m1.getOrDefault(ch, 0)+1);
+        // }
+
+        //  for(int i = 0; i < t.length(); i++){
+        //     char ch = t.charAt(i);
+        //     m2.put(ch, m2.getOrDefault(ch, 0)+1);
+        // }
+
+        // return m1.equals(m2);
 
         if(s.length() != t.length()) return false;
 
+        int [] sarr = new int [26];
+        int [] tarr = new int [26];
+
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            m1.put(ch, m1.getOrDefault(ch, 0)+1);
+            sarr[ch -  'a']++;
         }
 
-         for(int i = 0; i < t.length(); i++){
+        for(int i = 0; i < t.length(); i++){
             char ch = t.charAt(i);
-            m2.put(ch, m2.getOrDefault(ch, 0)+1);
+            tarr[ch -  'a']++;
         }
 
-        return m1.equals(m2);
+        for(int i = 0; i < 26; i++){
+            if(tarr[i] != sarr[i]) return false;
+        }
+
+        return true;
     }
 }
