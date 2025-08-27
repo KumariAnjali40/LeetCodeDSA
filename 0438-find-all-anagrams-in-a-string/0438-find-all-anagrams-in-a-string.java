a66f827bad04c784daaf728442c985692fdc175d
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-08-27 16:09:59 +0530

    Time: 9 ms (91.49%), Space: 44.8 MB (82.68%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,52 +1,100 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        HashMap<Character, Integer> pMap = new HashMap<>();
+        // HashMap<Character, Integer> pMap = new HashMap<>();
+
+        // int n = s.length();
+        // int m = p.length();
+        
+        // for(int i = 0; i < m; i++){
+        //     char ch = p.charAt(i);
+        //     pMap.put(ch, pMap.getOrDefault(ch,0)+1);
+        // }
+
+        // HashMap<Character, Integer> sMap = new HashMap<>();
+
+        // int left = 0; 
+        // int right = 0;
+        // int count = 0;
+
+        // List<Integer> list = new ArrayList<>();
+
+        // while(right < n){
+        //    char ch = s.charAt(right);
+        //    if(sMap.containsKey(ch)){
+        //      sMap.put(ch, sMap.get(ch)+1);
+        //    }else {
+        //     sMap.put(ch,1);
+        //    }
+
+        //    if(right - left + 1 < m){
+        //       right++;
+        //    } else if(right - left + 1 == m){
+        //       if(sMap.equals(pMap)){
+        //         list.add(left);
+        //       }
+
+
+        //       char leftChar = s.charAt(left);
+
+        //       sMap.put(leftChar, sMap.get(leftChar)-1);
+
+        //       if(sMap.get(leftChar) == 0){
+        //         sMap.remove(leftChar);
+        //       }
+
+        //       left++;
+        //       right++;
+        //    }
+        // }
+
+        // return list;
 
         int n = s.length();
         int m = p.length();
+
+        int [] parr = new int [26];
+        int [] sarr = new int [26];
         
         for(int i = 0; i < m; i++){
-            char ch = p.charAt(i);
-            pMap.put(ch, pMap.getOrDefault(ch,0)+1);
+            parr[p.charAt(i) - 'a']++;
         }
-
-        HashMap<Character, Integer> sMap = new HashMap<>();
-
-        int left = 0; 
-        int right = 0;
-        int count = 0;
 
         List<Integer> list = new ArrayList<>();
 
+        int left = 0; 
+        int right = 0;
+
         while(right < n){
-           char ch = s.charAt(right);
-           if(sMap.containsKey(ch)){
-             sMap.put(ch, sMap.get(ch)+1);
-           }else {
-            sMap.put(ch,1);
-           }
+            char ch = s.charAt(right);
 
-           if(right - left + 1 < m){
-              right++;
-           } else if(right - left + 1 == m){
-              if(sMap.equals(pMap)){
-                list.add(left);
-              }
+            sarr[ch - 'a']++;
 
+            if(right - left + 1 < m){
+                right++;
+            }else if( right - left + 1 == m){
+                if(areSame(parr, sarr)){
+                    list.add(left);
+                }
 
-              char leftChar = s.charAt(left);
+                char leftChar = s.charAt(left);
 
-              sMap.put(leftChar, sMap.get(leftChar)-1);
+                sarr[leftChar - 'a']--;
 
-              if(sMap.get(leftChar) == 0){
-                sMap.remove(leftChar);
-              }
-
-              left++;
-              right++;
-           }
+                left++;
+                right++;
+            }
         }
 
         return list;
+    }
+
+    public static boolean areSame(int [] s1,int [] s2){
+        for(int i = 0; i < 26; i++){
+            if(s1[i] != s2[i]){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
