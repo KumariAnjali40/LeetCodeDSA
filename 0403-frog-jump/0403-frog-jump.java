9dc4e32da659e2c6f2a4779eca317d16c87777c2
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-09-03 11:50:03 +0530

    Time: 44 ms (55.57%), Space: 48.8 MB (61.21%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,37 +1,21 @@
 class Solution {
     public boolean canCross(int[] stones) {
-        int n = stones.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < n; i++){
-            map.put(stones[i], i);
+        HashMap<Integer, Set<Integer>> map = new HashMap<>();
+        for(int stone : stones) {
+            map.put(stone, new HashSet<>());
         }
-        if(!map.containsKey(1)) return false;
+        map.get(0).add(0); // starting point, previous jump is 0
 
-        // Memoization: key = index + "-" + prevJump
-        HashMap<String, Boolean> memo = new HashMap<>();
-
-        return dfs(stones, 1, 1, map, memo);
-    }
-
-    private boolean dfs(int[] stones, int index, int prevJump, HashMap<Integer,Integer> map, HashMap<String, Boolean> memo) {
-        if(index == stones.length - 1) return true;
-
-        String key = index + "-" + prevJump;
-        if(memo.containsKey(key)) return memo.get(key);
-        for(int jump = prevJump - 1; jump <= prevJump + 1; jump++) {
-            if(jump <= 0) continue; 
-            int nextPos = stones[index] + jump;
-            if(map.containsKey(nextPos)) {
-                int nextIndex = map.get(nextPos);
-                if(dfs(stones, nextIndex, jump, map, memo)) {
-                    memo.put(key, true);
-                    return true;
+        for(int stone : stones) {
+            for(int jump : map.get(stone)) {
+                for(int nextJump = jump - 1; nextJump <= jump + 1; nextJump++) {
+                    if(nextJump > 0 && map.containsKey(stone + nextJump)) {
+                        map.get(stone + nextJump).add(nextJump);
+                    }
                 }
             }
         }
 
-        memo.put(key, false);
-        return false;
+        return !map.get(stones[stones.length - 1]).isEmpty();
     }
 }
