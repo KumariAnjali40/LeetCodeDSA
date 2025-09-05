e38f27396a5bea519bc7a19841450890594a41d7
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2025-09-05 11:57:34 +0530

    Time: 0 ms (100%), Space: 41.6 MB (8.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,31 +1,59 @@
 class Solution {
     public int rob(int[] nums) {
 
-        int [] dp = new int [nums.length+1];
+        // int [] dp = new int [nums.length+1];
 
-        for(int i = 0; i < nums.length; i++){
-            dp[i] = -1;
-        }
+        // for(int i = 0; i < nums.length; i++){
+        //     dp[i] = -1;
+        // }
         
-        int ans = robber(nums, 0, dp);
+        // int ans = robber(nums, 0, dp);
+
+        int n = nums.length;
+
+        if(n == 1) return nums[0];
+
+        int ans = solve(nums, n);
 
         return ans;
     }
 
-    public static int robber (int [] arr, int index, int [] dp){
+    public static int solve(int [] arr, int n){
 
-         if (index >= arr.length) {
-            return 0;
+        int prev = arr[0]; //dp[n-1]
+
+        int prev1 = 0; //dp[n-2]
+
+        for(int i = 1; i < n; i++){
+            int temp = arr[i];
+
+            int take = temp + prev1;
+            int nonTake = prev;
+
+            int curr = Math.max(take, nonTake);
+
+            prev1 = prev;
+            prev = curr;
         }
 
-        if (dp[index] != -1) {
-            return dp[index];
-        }
+        return prev;
 
-        int steal = arr[index] + robber(arr, index + 2, dp);
-        int skip = robber(arr, index + 1, dp);
-
-        dp[index] = Math.max(steal, skip);
-        return dp[index];
     }
+
+    // public static int robber (int [] arr, int index, int [] dp){
+
+    //      if (index >= arr.length) {
+    //         return 0;
+    //     }
+
+    //     if (dp[index] != -1) {
+    //         return dp[index];
+    //     }
+
+    //     int steal = arr[index] + robber(arr, index + 2, dp);
+    //     int skip = robber(arr, index + 1, dp);
+
+    //     dp[index] = Math.max(steal, skip);
+    //     return dp[index];
+    // }
 }
