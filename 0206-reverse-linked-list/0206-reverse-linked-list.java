b14e97b6bb84f50438a2e8a930c731e10fa41134
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2026-02-16 17:16:46 +0530

    Time: 0 ms (100%), Space: 44.6 MB (12.3%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,42 +10,42 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // ListNode prev = null;
-
-        // ListNode curr = head;
-
-        // while(curr != null){
-        //     ListNode next = curr.next;
-        //     curr.next = prev;
-        //     prev = curr;
-        //     curr = next;
-        // }
-
-        // return prev;
-
-        List<Integer> list = new ArrayList<>();
+        ListNode prev = null;
 
         ListNode curr = head;
 
         while(curr != null){
-            list.add(curr.val);
-            curr = curr.next;
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
 
-        if(list.size() == 0) {
-            return null;
-        }
+        return prev;
 
-        ListNode newHead = new ListNode(list.get(list.size()-1));
+        // List<Integer> list = new ArrayList<>();
 
-        ListNode newList = newHead;
+        // ListNode curr = head;
 
-        for(int i = list.size()-2; i >=0; i--){
-            newList.next = new ListNode(list.get(i));
-            newList = newList.next;
-        }
+        // while(curr != null){
+        //     list.add(curr.val);
+        //     curr = curr.next;
+        // }
 
-        return newHead;
+        // if(list.size() == 0) {
+        //     return null;
+        // }
+
+        // ListNode newHead = new ListNode(list.get(list.size()-1));
+
+        // ListNode newList = newHead;
+
+        // for(int i = list.size()-2; i >=0; i--){
+        //     newList.next = new ListNode(list.get(i));
+        //     newList = newList.next;
+        // }
+
+        // return newHead;
 
     }
 }
