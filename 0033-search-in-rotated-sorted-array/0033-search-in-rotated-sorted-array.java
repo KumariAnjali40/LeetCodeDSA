3bb9cc22bc50c0f4d9ae363de2de60bb6a99e6bb
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2026-03-06 17:36:53 +0530

    Time: 0 ms (100%), Space: 43.8 MB (82.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,32 +5,29 @@ class Solution {
         return searchInArr;
     }
 
-    public static int solve(int [] arr, int target){
-        
+    public static int solve(int [] arr, int k){
+
         int left = 0;
-        int right = arr.length-1;
+        int right = arr.length - 1; 
 
-        while(left <= right){
-         int mid = left + (right-left)/2;
+        while (left <= right){
+            int mid = left + (right - left)/2;
 
-         if(arr[mid] == target) return mid;
+            if(arr[mid] == k) return mid;
 
-         if(arr[left] <= arr[mid]){
-            //left part is sorted
-            if(target >= arr[left] && target < arr[mid]) {
-                right = mid-1;
-            } else {
-                left = mid+1;
-            }
-          }else {
-            //right part is sorted
-
-            if(target > arr[mid] && target <= arr[right]){
-                left = mid+1;
+            if(arr[left] <= arr[mid]){
+                if( k >= arr[left] && k < arr[mid]){
+                    right = mid-1;
+                }else {
+                    left = mid+1;
+                }
             }else {
-                right = mid-1;
+                if(k <= arr[right] && k > arr[mid]){
+                    left = mid+1;
+                }else {
+                    right = mid-1;
+                }
             }
-        }
         }
 
         return -1;
